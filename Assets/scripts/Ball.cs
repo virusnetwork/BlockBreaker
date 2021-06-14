ff7840391be0feb714b2c7098683f37a4dd98ca7
Miles <milessingleton@hotmail.co.uk>
2021-06-14 13:31:09 +0100

    locked ball to paddle

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // config params
+    [SerializeField] Paddle paddle1;
+
+    // state
+    Vector2 paddleToBallVector;
+
+    // Use this for initialization
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LockBallToPaddle();
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
