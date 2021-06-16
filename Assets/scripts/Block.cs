bfd442f9794786ce8cdcfe72e2ec789cd4571abc
Miles <milessingleton@hotmail.co.uk>
2021-06-16 11:07:45 +0100

    added score to game

--- Assets/scripts/Block.cs
+++ Assets/scripts/Block.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
@@ -19,8 +20,10 @@ public class Block : MonoBehaviour
 
     private void DestroyBlock()
     {
+        FindObjectOfType<GameStatus>().AddToScore();
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         FindObjectOfType<Level>().BlockDestroyed();
+        
     }
 }
--- Assets/scripts/GameStatus.cs
+++ Assets/scripts/GameStatus.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     //parameters
     [Range(0.5f,10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
+    //state variables
+    [SerializeField] int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = score.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
         Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        score += pointsPerBlockDestroyed;
+        scoreText.text = score.ToString();
     }
 }
